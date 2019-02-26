a38de03dc5fdb0540d047c113db06c53393db227
Christopher Stewart <christopherallenstewart@gmail.com>
2019-02-26 13:51:13 -0800

    setting up default settings for drupal

--- drupal/web/sites/default/default.settings.php
+++ drupal/web/sites/default/default.settings.php
@@ -768,3 +768,13 @@ $settings['entity_update_batch_size'] = 50;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+$databases['default']['default'] = array (
+  'database' => 'drupal8',
+  'username' => 'drupal8',
+  'password' => 'drupal8',
+  'host' => 'database',
+  'port' => '3306',
+  'driver' => 'mysql',
+  'prefix' => '',
+  'collation' => 'utf8mb4_general_ci',
+);
